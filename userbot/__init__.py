1070460ad6949edb8a128fa70ab969bdef488a8d
Aman Pandey Devloper <72003733+amanpandey7647@users.noreply.github.com>
2021-03-20 15:43:41 +0530

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -141,7 +141,7 @@ if bool(ENV):
     # Upstream Repo
     UPSTREAM_REPO_URL = os.environ.get(
     "UPSTREAM_REPO_URL",
-    "https://github.com/LEGENDXOP/LEGEND-BOT.git")
+    "https://github.com/amanpandey7647/Treylon-User-bot.git")
 
     # Last.fm Module
     BIO_PREFIX = os.environ.get("BIO_PREFIX", None)
