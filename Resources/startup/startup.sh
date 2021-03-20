ad3fe24cf6bb7df11db373b54ed0a6251f0f9365
Aman Pandey Devloper <72003733+amanpandey7647@users.noreply.github.com>
2021-03-20 13:17:44 +0530

    Create startup.sh

---
+++ Resources/startup/startup.sh
@@ -0,0 +1,14 @@
+  
+#!/bin/bash
+# Treylon - UserBot
+# Copyright (C) 2020 Treylon
+#
+# This file is a part of < https://github.com/amanpandey7647/treylon-user-bot >
+# PLease read the GNU Affero General Public License in <https://github.com/amanpandey7647/Treylon-User-bot/blob/main/LICENSE>.
+ 
+echo "
+        Treylon The Superuserbot
+        Visit - https://t.me/joinchat/8OTa7GGE6eJmNTg1 For Support
+"
+ 
+python3 -m pyTreylon
